9861690833402ccf32722fc1614185a989aa9e77
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-14 17:58:03 +0530

    master

--- src/main/scala/basics/AirthmeticOperators.scala
+++ src/main/scala/basics/AirthmeticOperators.scala
@@ -17,5 +17,110 @@ object AirthmeticOperators {
     println(s"Division is ${a / b}")
     //    Remainder
     println(s"Remainder is ${a % b}")
+
+
+    //    addition
+    println(s"Addition is ${a + b}")
+    //    subtraction
+    println(s"Subtraction is ${a - b}")
+    //    Multiplication
+    println(s"Multiplication is ${a * b}")
+    //    Division
+    println(s"Division is ${a / b}")
+    //    Remainder
+    println(s"Remainder is ${a % b}")
+
+    //    addition
+    println(s"Addition is ${a + b}")
+    //    subtraction
+    println(s"Subtraction is ${a - b}")
+    //    Multiplication
+    println(s"Multiplication is ${a * b}")
+    //    Division
+    println(s"Division is ${a / b}")
+    //    Remainder
+    println(s"Remainder is ${a % b}")
+
+
+    //    addition
+    println(s"Addition is ${a + b}")
+    //    subtraction
+    println(s"Subtraction is ${a - b}")
+    //    Multiplication
+    println(s"Multiplication is ${a * b}")
+    //    Division
+    println(s"Division is ${a / b}")
+    //    Remainder
+    println(s"Remainder is ${a % b}")
+
+
+    //    addition
+    println(s"Addition is ${a + b}")
+    //    subtraction
+    println(s"Subtraction is ${a - b}")
+    //    Multiplication
+    println(s"Multiplication is ${a * b}")
+    //    Division
+    println(s"Division is ${a / b}")
+    //    Remainder
+    println(s"Remainder is ${a % b}")
+
+
+    //    addition
+    println(s"Addition is ${a + b}")
+    //    subtraction
+    println(s"Subtraction is ${a - b}")
+    //    Multiplication
+    println(s"Multiplication is ${a * b}")
+    //    Division
+    println(s"Division is ${a / b}")
+    //    Remainder
+    println(s"Remainder is ${a % b}")
+
+
+    //    addition
+    println(s"Addition is ${a + b}")
+    //    subtraction
+    println(s"Subtraction is ${a - b}")
+    //    Multiplication
+    println(s"Multiplication is ${a * b}")
+    //    Division
+    println(s"Division is ${a / b}")
+    //    Remainder
+    println(s"Remainder is ${a % b}")
+
+    //    addition
+    println(s"Addition is ${a + b}")
+    //    subtraction
+    println(s"Subtraction is ${a - b}")
+    //    Multiplication
+    println(s"Multiplication is ${a * b}")
+    //    Division
+    println(s"Division is ${a / b}")
+    //    Remainder
+    println(s"Remainder is ${a % b}")
+
+    //    addition
+    println(s"Addition is ${a + b}")
+    //    subtraction
+    println(s"Subtraction is ${a - b}")
+    //    Multiplication
+    println(s"Multiplication is ${a * b}")
+    //    Division
+    println(s"Division is ${a / b}")
+    //    Remainder
+    println(s"Remainder is ${a % b}")
+
+    //    addition
+    println(s"Addition is ${a + b}")
+    //    subtraction
+    println(s"Subtraction is ${a - b}")
+    //    Multiplication
+    println(s"Multiplication is ${a * b}")
+    //    Division
+    println(s"Division is ${a / b}")
+    //    Remainder
+    println(s"Remainder is ${a % b}")
+
   }
 }
