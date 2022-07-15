e918d7effccc082a361dc23580678d274a0dd928
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-15 20:23:08 +0530

    master

--- src/main/scala/basics/for_loop2.scala
+++ src/main/scala/basics/for_loop2.scala
@@ -2,8 +2,7 @@ package basics
 
 object for_loop2 {
   def main(args: Array[String]): Unit = {
-    //    loop1()
-    pattern1(5)
+    rhombus_pattern(4)
   }
 
   def loop1(): Unit = {
@@ -15,7 +14,29 @@ object for_loop2 {
   }
 
   def pattern1(n: Int): Unit = {
-    for (i <- 1.to(5); j <- 1 to (i))
+    for (i <- 1.to(n); j <- 1 to i)
       if (i - j == 0) print("* \n") else print("* ")
   }
+
+  def pattern2(n: Int): Unit = {
+    for (i <- 1.to(n); j <- 1 to n)
+      if (i + j <= n) print("  ") else if (j == n) println("* ") else print("* ")
+  }
+
+  def pattern3(n: Int): Unit = {
+    for (i <- n.to(1) by -1; j <- 1.to(i))
+      if (j == i) println("* ") else print("* ")
+  }
+
+  def pattern4(n: Int): Unit = {
+    for (i <- n.to(1) by -1; j <- n.to(1) by -1)
+      if (j == 1) println("* ") else if (j <= i) print("* ") else print("  ")
+  }
+
+  def rhombus_pattern(n: Int): Unit = {
+    for (i <- 1.until(n * 2); j <- 1.until(n * 2))
+      if (i <= n) {if (i + j > n && j - i < n) if (j - i == n - 1) println("* ") else print("* ") else if (j <= n) print("  ")}
+      else {if (i - j <= n - 1 && i + j < (n * 3)) if (i + j == (n * 3) - 1) println("* ") else print("* ") else if (j <= n) print("  ")}
+  }
+
 }
