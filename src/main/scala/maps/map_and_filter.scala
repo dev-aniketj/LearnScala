d8e31d1d9a23b5050f1db2d2886b2b7e7fb5d32d
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-21 21:13:29 +0530

    master

--- src/main/scala/maps/map_and_filter.scala
+++ src/main/scala/maps/map_and_filter.scala
@@ -2,7 +2,7 @@ package maps
 
 object map_and_filter {
 
-  val list = List(2, 4, 6, 8, 10, 12)
+  var list = List(2, 4, 6, 8, 10, 12)
   val myMap = Map(1 -> "tom", 2 -> "max", 3 -> "john", 4 -> "aniket", 5 -> "ayush")
 
   def main(args: Array[String]): Unit = {
@@ -25,9 +25,14 @@ object map_and_filter {
     println("------------------------------------------------")
     println(List(List(1, 2, 3), List(3, 'a', 4, 5)).flatten)
     println(list.flatMap(x => List(x, x + 1)))
+    println(list.map(x => List(x, x + 1)))
 
+    //    Filter
 
-
+    println("\n--------------- Filter ---------------")
+    list = List(1, 2, 3, 4, 5, 6, 7, 8)
+    println(list.filter(x => x % 2 == 0)) //it gives the only Even Values.
+    println(list.filter(x => x % 2 != 0)) //it gives the only Odd Values.
 
   }
 }
